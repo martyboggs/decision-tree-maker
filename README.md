21f84ee7f89280ea8da59a0e0f46a1c2d3b0c93a
Martin Broder <m@thriveline.com>
2015-06-04 20:16:39 -0700

    updated readme, removed json version

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# decision-tree-maker
+# Decision Tree Maker
 pass an array of strings and get a clickable decision tree
+
+First, create your element to which Decision Tree Maker will be added:
+
+```
+<div id="tree"></div>
+```
+
+Include the script after jQuery and optionally Font Awesome:
+
+```
+<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<script src="decisionTreeMaker.js"></script>
+```
+
+Create your array in a JS file:
+
+```
+var a = [
+    '=Are you dead?',
+    '*You can stay home.',
+    '=Are you sick?',
+    '==Are you able to get out of bed?',
+    '*Go to work.',
+    '==Are you unable to get out of bed?',
+    '===Is it a hostage situation?',
+    '*Kill the bad guy. Go to work.',
+    '===Are you vomiting blood?',
+    '*Go to work.',
+    '=Are you well?',
+    '==Are you able to get out of bed?',
+    '*Go to work.',
+    '==Are you unable to get out of bed?',
+    '===Is it a hostage situation?',
+    '*Kill the bad guy. Go to work.',
+    '===Do you just not feel like it?',
+    '*Go to work.',
+];
+```
+
+Run it!
+Argument 1: Array of strings
+Argument 2: Id of HTML element
+
+```
+tree(a, 'tree');
+```
+
+You can use this CSS too
+
+```
+body {
+    font-size: 24px;
+    font-family: sans-serif;
+}
+a {
+    color: purple;
+    text-decoration: none;
+}
+a:hover {
+    color: black;
+}
+ul ul {
+    display: none;
+}
+ul li {
+    list-style-type: none;
+}
+i {
+    margin-right: 10px;
+}
+```
---
+++ decisionTreeMaker.js
@@ -0,0 +1,42 @@
+function tree(a, id) {
+    var container = $('#' + id);
+    container.append('<ul></ul>');
+    var previous = [container.find('ul')];
+    var level = 1;
+    for (var i = 0; i < a.length; i += 1) {
+        var equals = a[i].match(/^(=)+/);
+        var asterisk = a[i].match(/^\*/);
+        if (equals) {
+            var content = a[i].slice(equals[0].length);
+            if (equals[0].length === level) { // same level
+                previous[level-1].append('<li><a href="#"><i class="fa fa-arrow-circle-right"></i>' + content + '</a></li>');
+            } else if (equals[0].length - level === 1) { // next level
+                previous[level-1].find('li:last').append('<ul><li><a href="#"><i class="fa fa-arrow-circle-right"></i>' + content + '</a></li></ul>');
+                previous[level] = previous[level-1].find('ul:last');//problem
+                level += 1;
+            } else if (equals[0].length < level) { // back 1 or more levels
+                level = equals[0].length;
+                previous[level-1].append('<li><a href="#"><i class="fa fa-arrow-circle-right"></i>' + content + '</a></li>');
+            } else {
+                console.log('you skipped a level');
+            }
+        } else if (asterisk) {
+            var content = a[i].slice(asterisk[0].length);
+            previous[level-1].find('li:last').append('<ul><li>' + content + '</li></ul>');
+        } else {
+            console.log('parse error, each line must have = or * at the beginning');
+        }
+    }
+}
+
+$(function () {
+    $('a').on('click', function () {
+        if ($(this).next('ul').css('display') === 'none') {
+            $(this).next('ul').css('display', 'block');
+            $(this).children().first().removeClass('fa-arrow-circle-right').addClass('fa-arrow-circle-down');
+        } else {
+            $(this).next('ul').css('display', 'none');
+            $(this).children().first().addClass('fa-arrow-circle-right').removeClass('fa-arrow-circle-down');
+        }
+    });
+});
